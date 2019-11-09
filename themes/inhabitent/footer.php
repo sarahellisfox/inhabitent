e610232a9305d152aa042e609de082f4ad9cff58
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-08 18:23:55 -0800

    added footer and contact content

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -1,22 +1,33 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * @package RED_Starter_Theme
- */
+</div><!-- #content -->
 
-?>
+	<footer id="site-footer" class="site-footer" role="contentinfo">
+		
+		<div class="div-contact">
+			<h3>Contact Info</h3> 
+			<p>info@inhabitent.com</p> 
+			<p>778-456-7891</p> 
+			<!-- Social Media Icons  -->
+		</div> 
 
-			</div><!-- #content -->
+		<div class="div-hours">
+			<h3>Business Hours</h3> 
+			<p>Monday-Friday: 9am to 5pm</p> 
+			<p>Saturday: 10am to 2pm</p>
+			<p>Sunday: Closed</p>
+		</div> 
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
+		<div class="footer-logo">
+
+		</div> 
+			<img src="/images/logos/inhabitent-logo-tent-white.svg">
+		
+		<div class="footer-copyright">
+			<p>COPYRIGHT © 2019 INHABITENT</p>	
+		</div>
+
+	</footer>
+
 		</div><!-- #page -->
-
-		<?php wp_footer(); ?>
-
+		
 	</body>
 </html>
--- themes/inhabitent/gulpfile.js
+++ themes/inhabitent/gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('browser-sync', function() {
   ];
 
   browserSync.init(files, {
-    proxy: 'localhost:8888/Inhabitant'
+    proxy: 'localhost:8888/inhabitent'
   });
 
   gulp.watch(files).on('change', browserSync.reload);
