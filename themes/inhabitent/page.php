79622b535460c002575b1ba2ccd21e74f2b2f85e
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-17 13:26:57 -0800

    Finished styling of Find Us page

--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -14,11 +14,9 @@ get_header(); ?>
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content'); ?>
+				<?php get_template_part( 'template-parts/content-page'); ?>
 
 			<?php endwhile; // End of the loop. ?>
-			
-				</div>
 
 		</main><!-- #main -->
 
