a26148dc16a40fd778ee8ac9a277c579abcc0922
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-17 10:59:10 -0800

    fixed alignment and sizing on Front Page

--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -129,6 +129,6 @@ function tt_hidetitle_class($classes) {
 	// Defines new size for thumbnail picture 
 
 	add_theme_support( 'post-thumbnails' ); 
-	add_image_size( 'feature-post size', 350, 225 );
+	add_image_size( 'feature-post size', 352, auto);
 	add_image_size( 'banner size', 4000, 2667 ); 
 
