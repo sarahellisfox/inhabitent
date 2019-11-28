5633ed023f0990a9b82ae5a05b01afea3d74d9e1
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-27 22:31:56 -0800

    tweak css

--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -5,7 +5,7 @@
 			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
 		</button>
 		<label>
-			<input type="search" class="searchField" id="searchField" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+			<input type="search" class="searchfield" id="searchField" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 	</fieldset>
 </form>
